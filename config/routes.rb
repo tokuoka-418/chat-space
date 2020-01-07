00f18ef20fe97c06c778e81f92898f08600847a0
徳岡 <vivio.hayate.nanoha@i.softbank.jp>
2020-01-07 16:51:21 +0900

    group

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-   # ログアウト
-   devise_scope :social_account do
-    get 'sign_out', to: "sessions#destroy"
-  end
 
 root "messages#index"
 resources :users, only: [:edit, :update]
+root "groups#new"
 end
