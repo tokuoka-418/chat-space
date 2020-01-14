ea066d783f09eaf83b0d3f338e4b04689b2bf672
徳岡 <vivio.hayate.nanoha@i.softbank.jp>
2020-01-14 15:01:44 +0900

    group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
    
@@ -36,5 +37,9 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group 
+    @group = Group.find(params[:id])
   end
 end
