17421c816536443c3703de84773d6f008ebdb02e
徳岡 <vivio.hayate.nanoha@i.softbank.jp>
2020-01-28 16:07:13 +0900

    Automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -73,24 +73,19 @@ $(function(){
       data: {id: last_message_id}
       .done(function(messages) {
         if (messages.length !== 0) {
-        var insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
         });
-        $('.messages').append(insertHTML);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-        $("#new_message")[0].reset();
-        $(".form__submit").prop("disabled", false);
-      }
-  
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
       })
       .fail(function() {
-      
+        alert("通信エラーです。");
       })
-
-    
-
     })
-  
   }
 });
