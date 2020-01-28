2ff6e55ba18297e9e00bf1bfd818204068acc354
徳岡 <vivio.hayate.nanoha@i.softbank.jp>
2020-01-28 15:30:09 +0900

    Automatic

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ resources :messages, only: [:index]
 resources :users, only: [:edit, :update, :index]
 resources :groups, only: [:new, :create, :edit, :update] do
   resources :messages,only: [:index,:create]
+  namespace :api do
+    resources :messages, only: :index, defaults: { format: 'json' }
+  end
 end
 end
